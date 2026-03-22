360f0db17d6da8380c78969d470df3471454ad44
Jachym Holecek <holecekjachym5@gmail.com>
2026-03-22 12:29:08 +0100

    Disable authentication/authorizatin for now

--- sauron_api/lib/SauronAPI.pm
+++ sauron_api/lib/SauronAPI.pm
@@ -26,26 +26,26 @@ sub startup {
     route => $self->routes->any('/api/v1'),
     schema => 'v3',
     skip_validating_specification => 1,
-    security => {
-      ApiKeyAuth => sub ($c, $definition, $scopes, $cb) {
-        my $provided_key = $c->req->headers->header('X-API-KEY');
-        if ($provided_key && exists $config->{api_keys}->{$provided_key}) {
-          my $key_data = $config->{api_keys}->{$provided_key};
-          my $user_role = $key_data->{role};
-          print "user role from config: '$user_role'\n";
-          print "user role from HTTP: '@$scopes'\n";
-          my $is_authorized = grep { $_ eq $user_role } @$scopes;
-          return $c->$cb("Forbidden: You do not have permissions to perform this action") unless $is_authorized;
-          $c->stash(
-            api_user => $key_data->{owner},
-            api_role => $key_data->{role}
-          );
-
-          return $c->$cb();
-        }
-        return $c->$cb("Invalid or missing API key");
-      }
-    }
+    # security => {
+    #   ApiKeyAuth => sub ($c, $definition, $scopes, $cb) {
+    #     my $provided_key = $c->req->headers->header('X-API-KEY');
+    #     if ($provided_key && exists $config->{api_keys}->{$provided_key}) {
+    #       my $key_data = $config->{api_keys}->{$provided_key};
+    #       my $user_role = $key_data->{role};
+    #       print "user role from config: '$user_role'\n";
+    #       print "user role from HTTP: '@$scopes'\n";
+    #       my $is_authorized = grep { $_ eq $user_role } @$scopes;
+    #       return $c->$cb("Forbidden: You do not have permissions to perform this action") unless $is_authorized;
+    #       $c->stash(
+    #         api_user => $key_data->{owner},
+    #         api_role => $key_data->{role}
+    #       );
+    #
+    #       return $c->$cb();
+    #     }
+    #     return $c->$cb("Invalid or missing API key");
+    #   }
+    # }
   });
 
   $self->plugin(SwaggerUI => {
